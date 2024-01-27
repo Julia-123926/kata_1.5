b6ec4190b331d31b07877765475d09b1239becdc
Julia Gogonova <gogonova2010@yandex.ru>
2024-01-27 19:17:08 +0300

    block5

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  //   direction: "vertical",
+  //     loop: true,
+
+  //   spaceBetween: 30,
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //   },
+  slidesPerView: "auto",
+//   spaceBetween: 16,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
